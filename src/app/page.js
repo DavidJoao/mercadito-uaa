b82e3e6330d02986b69b64a8544bf37ee4cb6689
DavidJoao <davidsandoval596@gmail.com>
2024-09-09 20:32:54 -0700

    ADD Buttons for Login and Register

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import "./globals.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,8 +10,8 @@ export default function Home() {
         <p className="text-center"> Este mercado tiene como objetivo principal favorecer a los estudiantes, ofreciendo una plataforma donde puedan comprar y vender productos y servicios de manera segura y conveniente. Tu participación es crucial para entender mejor tus necesidades y preferencias. ¡Gracias por tu colaboración!</p>
       </div>
       <div className="w-[90%] md:w-[550px] flex flex-col md:flex-row items-center justify-evenly p-5 gap-5">
-        <button className="blue-button text-white w-[150px]">Iniciar Sesión</button>
-        <button className="blue-button text-white w-[150px]">Crear Cuenta</button>
+        <Link href={'/login'} className="blue-button text-white w-[150px]">Iniciar Sesión</Link>
+        <Link href={'/signup'} className="blue-button text-white w-[150px]">Crear Cuenta</Link>
       </div>
     </div>
   );
