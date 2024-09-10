b4848e40de6cb124bf4f9dbe1367fa19dceab7ab
DavidJoao <davidsandoval596@gmail.com>
2024-09-09 20:33:07 -0700

    CREATE Page for login

---
+++ src/app/login/page.js
@@ -0,0 +1,19 @@
+import Link from "next/link"
+
+const page = () => {
+  return (
+    <div className='main-background w-screen h-screen flex flex-col items-center justify-center'>
+        <h1 className="font-bold text-2xl text-white mb-4">Mercadito Universitario: UAA</h1>
+        <div className="w-[90%] h-[50%] md:h-auto md:w-[550px] p-5 bg-white/70 rounded shadow-2xl flex flex-col justify-center gap-2">
+            <label>Email</label>
+            <input placeholder="ejemplo@gmail.com" className="input" />
+            <label>Contraseña</label>
+            <input placeholder="*********" className="input" type="password" />
+            <button className="mt-5 blue-button text-white">Iniciar Sesión</button>
+            <Link className="mt-5 text-center underline" href={'/signup'}>No tienes una cuenta? Crea una aquí</Link>
+        </div>
+    </div>
+  )
+}
+
+export default page
