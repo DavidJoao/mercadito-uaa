83cf0c508027543079357656ee1a3264ef9b1226
DavidJoao <davidsandoval596@gmail.com>
2024-09-10 19:48:49 -0700

    CREATED accions/auth and validation for signup form

---
+++ src/app/actions/auth.js
@@ -0,0 +1,22 @@
+import { SignupFormSchema } from "../lib/definitions";
+
+export async function signup (state, formData) {
+
+    ///////////////////////////////////////////////////// FIELDS VALIDATION
+
+    const validatedFields = SignupFormSchema.safeParse({
+        name: formData.get('name'),
+        email: formData.get('email'),
+        password: formData.get('password')
+    })
+
+    if (!validatedFields.success) {
+        return { 
+            errors: validatedFields.error.flatten().fieldErrors,
+        }
+    }
+
+
+    
+
+}
