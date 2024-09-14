0fe254ebd9ac6e8022a2cb93ce803201de160d2d
DavidJoao <davidsandoval596@gmail.com>
2024-09-13 22:03:35 -0700

    ADDED Functionality to login page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -2,41 +2,52 @@
 
 import Link from "next/link"
 import { useState } from "react"
+import { authenticate, logSession } from "../lib/actions/session"
+import { useRouter } from "next/navigation"
 
 const page = () => {
+  ///////////////////////////////////////////// VARIABLES
+
+  const router = useRouter()
 
   ///////////////////////////////////////////// INITIAL OBJECTS
 
-  const initialForm = {
+  const initialFormData = {
     email: "",
     password: "",
   }
 
     ///////////////////////////////////////////// STATES
     
-    const [form, setForm] = useState(initialForm)
+    const [formData, setFormData] = useState(initialFormData)
     
     ///////////////////////////////////////////// HANDLERS
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setForm({
-      ...form,
+    setFormData({
+      ...formData,
       [name] : value
     })
+  }
+
+  const handleAuthentication = (e) => {
+    authenticate(formData)
+      .then(res => console.log(res))
+    router.refresh();
   }
 
   return (
     <div className='main-background w-screen h-screen flex flex-col items-center justify-center'>
         <h1 className="font-bold text-2xl text-white mb-4">Mercadito Universitario: UAA</h1>
-        <div className="w-[90%] h-[50%] md:h-auto md:w-[550px] p-5 bg-white/70 rounded shadow-2xl flex flex-col justify-center gap-2">
+        <form className="w-[90%] h-[50%] md:h-auto md:w-[550px] p-5 bg-white/70 rounded shadow-2xl flex flex-col justify-center gap-2" action={handleAuthentication}>
             <label>Email</label>
             <input required name="email" placeholder="ejemplo@gmail.com" className="input" onChange={handleChange}/>
             <label>Contraseña</label>
             <input required name="password"placeholder="*********" className="input" type="password" onChange={handleChange}/>
             <button className="mt-5 blue-button text-white">Iniciar Sesión</button>
             <Link className="mt-5 text-center underline" href={'/signup'}>No tienes una cuenta? Crea una aquí</Link>
-        </div>
+        </form>
     </div>
   )
 }
