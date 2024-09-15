5926f28f4629e2855f3e759536ec57747339ad7a
DavidJoao <davidsandoval596@gmail.com>
2024-09-15 12:10:07 -0700

    CREATED HandleSubmit Function in signup page

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -2,6 +2,7 @@
 
 import Link from "next/link"
 import { useState } from "react"
+import { registerUser } from "../lib/actions/auth"
 
 const page = () => {
 
@@ -28,11 +29,15 @@ const page = () => {
         })
     }
 
+    const handleSubmit = (e) => {
+        registerUser(form)
+    }
+
   return (
     <div className='main-background w-screen h-screen flex flex-col items-center justify-center'>
         <h1 className="font-bold text-2xl text-white mb-4">Mercadito Universitario: UAA</h1>
         <div className="w-[90%] h-[50%] md:h-auto md:w-[550px] p-5 bg-white/70 rounded shadow-2xl flex flex-col justify-center gap-2">
-            <form className="flex flex-col">
+            <form className="flex flex-col" action={handleSubmit}>
                 <label htmlFor="email">Email</label>
                 <input required name="email" id="email" placeholder="ejemplo@gmail.com" className="input" onChange={handleChange}/>
                 <label htmlFor="name">Nombre</label>
