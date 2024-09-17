e76e115c19cc7ac99a0eacf5dd412ae81eebb059
DavidJoao <davidsandoval596@gmail.com>
2024-09-16 22:04:01 -0700

    CHANGE Login Page / Changed handleAuthentication function

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -32,6 +32,7 @@ const page = () => {
   }
 
   const handleAuthentication = (e) => {
+    e.preventDefault();
     authenticate(formData)
       .then(res => console.log(res))
     router.refresh();
@@ -40,12 +41,12 @@ const page = () => {
   return (
     <div className='main-background w-screen h-screen flex flex-col items-center justify-center'>
         <h1 className="font-bold text-2xl text-white mb-4">Mercadito Universitario: UAA</h1>
-        <form className="w-[90%] h-[50%] md:h-auto md:w-[550px] p-5 bg-white/70 rounded shadow-2xl flex flex-col justify-center gap-2" action={handleAuthentication}>
+        <form className="w-[90%] h-[50%] md:h-auto md:w-[550px] p-5 bg-white/70 rounded shadow-2xl flex flex-col justify-center gap-2" onSubmit={handleAuthentication}>
             <label>Email</label>
             <input required name="email" placeholder="ejemplo@gmail.com" className="input" onChange={handleChange}/>
             <label>Contraseña</label>
             <input required name="password"placeholder="*********" className="input" type="password" onChange={handleChange}/>
-            <button className="mt-5 blue-button text-white">Iniciar Sesión</button>
+            <button type="submit" className="mt-5 blue-button text-white">Iniciar Sesión</button>
             <Link className="mt-5 text-center underline" href={'/signup'}>No tienes una cuenta? Crea una aquí</Link>
         </form>
     </div>
