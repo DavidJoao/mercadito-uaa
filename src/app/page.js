972cac1def22e07efd0fb4d3799789a05ad5d6e0
DavidJoao <davidsandoval596@gmail.com>
2024-09-20 21:13:54 -0700

    EDITED Principal Page

--- src/app/page.js
+++ src/app/page.js
@@ -4,14 +4,18 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="main-background h-screen w-screen flex flex-col items-center justify-center gap-3">
-      <div className="w-[90%] md:w-[550px] p-5 rounded bg-white/60 flex flex-col justify-center shadow-2xl">
-        <h1 className="font-bold text-2xl text-center">Mercadito Virtual: UAA</h1>
-        <p className="text-center"> Este mercado tiene como objetivo principal favorecer a los estudiantes, ofreciendo una plataforma donde puedan comprar y vender productos y servicios de manera segura y conveniente. Tu participación es crucial para entender mejor tus necesidades y preferencias. ¡Gracias por tu colaboración!</p>
+    <div className="main-background h-screen w-screen flex flex-col md:flex-row">
+      <div className="w-screen md:w-[60%] flex items-center justify-center p-5">
+        <div className="w-[90%] md:w-[550px] p-5 rounded-3xl bg-white/60 flex flex-col justify-center shadow-2xl">
+          <h1 className="font-bold text-2xl text-center">Mercadito Virtual: UAA</h1>
+          <p className="text-center"> Este mercado tiene como objetivo principal favorecer a los estudiantes, ofreciendo una plataforma donde puedan comprar y vender productos y servicios de manera segura y conveniente. Tu participación es crucial para entender mejor tus necesidades y preferencias. ¡Gracias por tu colaboración!</p>
+        </div>
       </div>
-      <div className="w-[90%] md:w-[550px] flex flex-col md:flex-row items-center justify-evenly p-5 gap-5">
-        <Link href={'/pages/login'} className="blue-button text-white w-[150px]">Iniciar Sesión</Link>
-        <Link href={'/pages/signup'} className="blue-button text-white w-[150px]">Crear Cuenta</Link>
+      <div className="w-screen md:w-[40%] flex items-center justify-center">
+        <div className="w-[90%] md:w-[550px] flex flex-col items-center justify-evenly p-5 gap-10">
+          <Link href={'/pages/login'} className="large-blue-button text-white w-[320px]">Iniciar Sesión</Link>
+          <Link href={'/pages/signup'} className="large-blue-button text-white w-[320px]">Crear Cuenta</Link>
+        </div>
       </div>
     </div>
   );
