900c3fa3e40f462d2a6c8d0579da50db9968fe62
DavidJoao <davidsandoval596@gmail.com>
2024-09-21 22:19:12 -0700

    CREATED Navbar component

---
+++ src/app/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='border-[1px] border-black w-screen h-[40px] fixed pl-[20px] pr-[20px]'>
+
+    </div>
+  )
+}
+
+export default Navbar
