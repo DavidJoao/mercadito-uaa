0ba9c8927dd321269cd8dbe54e7fa34385e0413c
DavidJoao <davidsandoval596@gmail.com>
2024-09-21 19:38:44 -0700

    CHANGED Login Form, Now showing description above login form

--- src/app/pages/login/page.js
+++ src/app/pages/login/page.js
@@ -77,7 +77,10 @@ const page = () => {
 
   return (
     <div className='main-background w-screen h-screen flex flex-col items-center justify-center'>
-        <h1 className="font-bold text-2xl text-white mb-4">Mercadito Universitario: UAA</h1>
+          <div className="w-[90%] md:w-[550px] p-5 rounded-3xl bg-white/60 flex flex-col justify-center shadow-2xl mb-3">
+            <h1 className="font-bold text-2xl text-center">Mercadito Virtual: UAA</h1>
+            <p className="text-center"> Este mercado tiene como objetivo principal favorecer a los estudiantes, ofreciendo una plataforma donde puedan comprar y vender productos y servicios de manera segura y conveniente. Tu participación es crucial para entender mejor tus necesidades y preferencias. ¡Gracias por tu colaboración!</p>
+          </div>
         <form className="w-[90%] h-[50%] md:h-auto md:w-[550px] p-5 bg-white/70 rounded shadow-2xl flex flex-col justify-center gap-2" onSubmit={(e) => handleAuthentication(e)}>
             <label>Email</label>
             <input required name="email" placeholder="ejemplo@gmail.com" className="input" onChange={handleChange}/>
