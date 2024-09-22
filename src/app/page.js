8e672867885fe68961e71354309e378df40d6bac
DavidJoao <davidsandoval596@gmail.com>
2024-09-21 19:38:16 -0700

    CHANGED Principal page and moved description of web app to login page

--- src/app/page.js
+++ src/app/page.js
@@ -4,14 +4,10 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="main-background h-screen w-screen flex flex-col md:flex-row">
-      <div className="w-screen md:w-[60%] flex items-center justify-center p-5">
-        <div className="w-[90%] md:w-[550px] p-5 rounded-3xl bg-white/60 flex flex-col justify-center shadow-2xl">
-          <h1 className="font-bold text-2xl text-center">Mercadito Virtual: UAA</h1>
-          <p className="text-center"> Este mercado tiene como objetivo principal favorecer a los estudiantes, ofreciendo una plataforma donde puedan comprar y vender productos y servicios de manera segura y conveniente. Tu participación es crucial para entender mejor tus necesidades y preferencias. ¡Gracias por tu colaboración!</p>
-        </div>
+    <div className="h-screen w-screen flex flex-col-reverse md:flex-row md:flex-row">
+      <div className="w-screen md:w-[60%] h-[40%] md:h-auto bg-[url('https://www.uaa.mx/portal/wp-content/uploads/2022/11/357-1024x683.jpg')] bg-no-repeat bg-cover border-[10px] border-[#002d75] shadow-right">
       </div>
-      <div className="w-screen md:w-[40%] flex items-center justify-center">
+      <div className="w-screen md:w-[40%] h-[60%] md:h-screen flex items-center justify-center main-background">
         <div className="w-[90%] md:w-[550px] flex flex-col items-center justify-evenly p-5 gap-10">
           <Link href={'/pages/login'} className="large-blue-button text-white w-[320px]">Iniciar Sesión</Link>
           <Link href={'/pages/signup'} className="large-blue-button text-white w-[320px]">Crear Cuenta</Link>
