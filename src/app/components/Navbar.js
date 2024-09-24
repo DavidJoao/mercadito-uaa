c546e3d3085eb93a71a186281008a912600d0d7d
DavidJoao <davidsandoval596@gmail.com>
2024-09-23 19:59:22 -0700

    ADDED Logout button to navbar

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,9 +1,12 @@
+"use client"
 import React from 'react'
+import { logoutUser } from '../lib/actions/session'
+import { IoIosLogOut } from 'react-icons/io'
 
 const Navbar = () => {
   return (
-    <div className='border-[1px] border-black w-screen h-[40px] fixed pl-[20px] pr-[20px]'>
-
+    <div className='border-[1px] border-black w-screen h-[40px] fixed pl-[20px] pr-[20px] flex flex-row-reverse items-center'>
+      <button><IoIosLogOut className='text-xl float' onClick={() => logoutUser()}>Logout</IoIosLogOut></button>
     </div>
   )
 }
